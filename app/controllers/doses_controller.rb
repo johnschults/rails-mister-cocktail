0973eae133eb55f01246cd190a321911adfbaec6
John Schults <johnschults@hotmail.com>
2019-11-14 23:19:22 +0100

    updating cocktails

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,8 +1,8 @@
 class DosesController < ApplicationController
-   # def new
-  #   @cocktail = Cocktail.find(params[:cocktail_id])
-  #   @dose = Dose.new
-  # end
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
@@ -11,7 +11,8 @@ class DosesController < ApplicationController
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      @review = Review.new
+      # @review = Review.new
+      @cocktail = Cocktail.new
       render 'cocktails/show'
     end
   end
