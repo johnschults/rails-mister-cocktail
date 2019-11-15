d8adb739c8cec1bafe98d794084c59ff656daa77
John Schults <johnschults@hotmail.com>
2019-11-15 15:06:41 +0100

    Add dotenv - Protect my secret data in .env file

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,5 +2,5 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, presence: true
-  validates :cocktail, uniqueness: { scope: :ingredient }
+  validates_uniqueness_of :cocktail, scope: :ingredient
 end
